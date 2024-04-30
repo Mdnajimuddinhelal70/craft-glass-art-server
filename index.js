a7b58b29d583b9eb0b58bdee3ba29cdec3f59e3e
najim77288 <najimuddin77288@gmail.com>
2024-04-30 21:10:44 +0600

    finished

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    
     console.log("Connected successfully to MongoDB server");
 
     // collection
