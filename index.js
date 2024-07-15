4fc77268297e2104bfcc45bdadf6cdb813e66dc5
najim77288 <najimuddin77288@gmail.com>
2024-07-16 00:02:23 +0600

    added api for getting details

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express = require("express");
-const cors = require("cors");
-require('dotenv').config()
+const express = require('express')
 const app = express();
-const port = process.env.PORT || 5001;
+const cors = require('cors');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
 
 
-// middleware
+
+//middleware 
 app.use(cors());
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.faaeiem.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a7x3bqu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-console.log(uri)
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -22,85 +22,44 @@ const client = new MongoClient(uri, {
   }
 });
 
-
 async function run() {
   try {
-    
-    console.log("Connected successfully to MongoDB server");
+   
+    const craftsCollection = client.db("craftsDb").collection("crafts");
 
-    // collection
-      const craftCollection = client.db('craftDB').collection('craft');
-      const userCollection = client.db('craftDB').collection('user')
 
-// data gulake get korar system such as
-app.get('/craft', async(req, res) => {
-  const cursor = craftCollection.find();
-  const result = await cursor.toArray();
-  res.send(result)
-})
-
-    app.post('/craft', async(req, res) => {
-      const newCraft = req.body;
-      console.log(newCraft)
-      const result = await craftCollection.insertOne(newCraft);
+    app.get('/crafts', async(req, res) => {
+      const result = await craftsCollection.find().toArray();
       res.send(result)
     })
 
-    app.get("/", (req, res) => {
-      res.send("craft making is running");
-    });
-    app.listen(port, () => {
-      console.log(`coffee server is running on port ${port}`);
-    });
-
-    // For delete
-    app.delete('/craft/:id', async(req, res) => {
+    // for showing details page 
+    // app.get('/productDetails/:id', async (req, res) => {
+    //   const id = req.params.id;
+    //   const craft = await craftsCollection.findOne({_id: new ObjectId(id) });
+    //   res.send(craft);
+    // });
+    app.get('/productDetails/:id', async(req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-      const result = await craftCollection.deleteOne(query);
-      res.send(result);
+      const details = await craftsCollection.findOne({_id: new ObjectId(id)})
+      res.send(details)
     })
 
-  // For update
-  app.get('/craft/:id', async(req, res) => {
-    const id = req.params.id;
-    const query = {_id: new ObjectId(id)}
-    const result = await craftCollection.findOne(query)
-    res.send(result);
-  })
-
-  app.post('/user', async(req, res) => {
-    const user = req.body;
-    console.log(user)
-    const result = await userCollection.insertOne(user);
-    res.send(result);
-  })
-
-
-  app.put('/craft/:id', async(req, res) => {
-    const id = req.params.id;
-    const filter = {_id: new ObjectId(id)}
-    const option = {upsert: true};
-    const updatedCraft = req.body;
-    const craft = {
-      $set: {
-        image: updatedCraft.image,
-         itemName: updatedCraft.itemName,
-         subcategoryName: updatedCraft.subcategoryName,
-         price: updatedCraft.price,
-        rating: updatedCraft.rating,
-        processingTime: updatedCraft.processingTime,
-         description: updatedCraft.description,
-      }
-    }
-    const result = await craftCollection.updateOne(filter, craft, option);
-    res.send(result)
-  })
-
-  } catch (error) {
-    console.log("Error connecting to MongoDB:", error.message);
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+  
   }
 }
+run().catch(console.dir);
 
 
-run();
+
+app.get('/', (req, res) => {
+  res.send('craft is running')
+})
+
+app.listen(port, () => {
+  console.log(`Craft and arts are running on port ${port}`)
+})
+
