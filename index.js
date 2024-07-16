0fa964289b0e68de90c0e56207068a4e1a1cc3f7
najim77288 <najimuddin77288@gmail.com>
2024-07-16 11:00:53 +0600

    added form data to the server side

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ async function run() {
   try {
    
     const craftsCollection = client.db("craftsDb").collection("crafts");
+    const formProductCollection = client.db("craftsDb").collection("formProduct");
 
 
     app.get('/crafts', async(req, res) => {
@@ -33,16 +34,18 @@ async function run() {
       res.send(result)
     })
 
-    // for showing details page 
-    // app.get('/productDetails/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const craft = await craftsCollection.findOne({_id: new ObjectId(id) });
-    //   res.send(craft);
-    // });
     app.get('/productDetails/:id', async(req, res) => {
       const id = req.params.id;
       const details = await craftsCollection.findOne({_id: new ObjectId(id)})
       res.send(details)
+    })
+
+    //API for form product
+    app.post('/formProduct', async(req, res) => {
+      console.log(req.body)
+      const result = await formProductCollection.insertOne(req.body);
+      console.log(result);
+      res.send(result)
     })
 
     await client.db("admin").command({ ping: 1 });
